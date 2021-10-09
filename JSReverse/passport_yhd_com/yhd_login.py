47d42153c790952db0e5ecb02cb3f47a7ba120ee
TRHX <admin@itrhx.com>
2021-10-09 18:03:11 +0800

    ✅ 1号店登录【2021.10.09】

--- JSReverse/passport_yhd_com/yhd_login.py
+++ JSReverse/passport_yhd_com/yhd_login.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# ==================================
+# --*-- coding: utf-8 --*--
+# @Time    : 2021-10-09
+# @Author  : TRHX
+# @Blog    : www.itrhx.com
+# @CSDN    : itrhx.blog.csdn.net
+# @FileName: yhd_login.py
+# @Software: PyCharm
+# ==================================
 
 
 import execjs
